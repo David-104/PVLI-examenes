5b3c52f1a0d374ac83d9ac30a489694f0e86d9f6
David-104 <dcanelad@ucm.es>
2024-01-09 09:17:58 +0100

    assets examen

---
+++ Examen PVLI/src/game.js
@@ -0,0 +1,30 @@
+let config = {
+	type: Phaser.AUTO,
+	parent: 'juego',
+	pixelArt: true,
+	scale: {
+		autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+		mode: Phaser.Scale.FIT,
+		width: 512,
+		height: 512,
+		zoom: 1
+	},
+	scene: [],
+	physics: { 
+		default: 'arcade', 
+		arcade: { 
+			debug: true 
+		},
+		checkCollision: {
+			up: true,
+			down: true,
+			left: true,
+			right: true
+		}
+	},
+	title: "PVLI Ordinaria 23/24",
+	version: "1.0.0",
+	transparent: false
+};
+
+new Phaser.Game(config);
